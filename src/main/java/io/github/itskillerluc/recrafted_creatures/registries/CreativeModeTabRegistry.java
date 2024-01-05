22fe584a3175124114a3ce47d1fd11d8c0138b0d
Itskillerluc <lvgraef@zeelandnet.nl>
2024-01-05 14:38:55 +0100

    Added paintings to creative tab
    Added the mods painting variants to the creative mode inventory tab.

--- src/main/java/io/github/itskillerluc/recrafted_creatures/registries/CreativeModeTabRegistry.java
+++ src/main/java/io/github/itskillerluc/recrafted_creatures/registries/CreativeModeTabRegistry.java
@@ -2,10 +2,13 @@ package io.github.itskillerluc.recrafted_creatures.registries;
 
 import io.github.itskillerluc.recrafted_creatures.RecraftedCreatures;
 import net.minecraft.Util;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.tags.InstrumentTags;
+import net.minecraft.world.entity.decoration.Painting;
+import net.minecraft.world.entity.decoration.PaintingVariant;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -13,11 +16,15 @@ import net.minecraft.world.item.Items;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.Comparator;
+
 import static net.minecraft.world.item.CreativeModeTabs.generateInstrumentTypes;
 
 public class CreativeModeTabRegistry {
     public static DeferredRegister<CreativeModeTab> CREATIVEMODE_TAB_REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, RecraftedCreatures.MODID);
-
+    private static final Comparator<Holder<PaintingVariant>> PAINTING_COMPARATOR = Comparator.comparing(Holder::value, Comparator.<PaintingVariant>comparingInt((p_270004_) -> {
+        return p_270004_.getHeight() * p_270004_.getWidth();
+    }).thenComparing(PaintingVariant::getWidth));
     public static final RegistryObject<CreativeModeTab> RECRAFTED_TAB = CREATIVEMODE_TAB_REGISTRY.register("recrafted_creatures",
             () -> CreativeModeTab.builder()
                     .title(Component.translatable("itemGroup." + RecraftedCreatures.MODID + ".tab"))
@@ -34,5 +41,12 @@ public class CreativeModeTabRegistry {
                                         item != ItemRegistry.LEAF_PICKER.get() &&
                                         item != ItemRegistry.SMALL_LEAF.get())
                                 .forEach(out::accept);
+
+                        PaintingRegistry.PAINTINGS.getEntries().stream().map(entry -> entry.getHolder().get()).sorted(PAINTING_COMPARATOR).forEach((p_269979_) -> {
+                            ItemStack itemstack = new ItemStack(Items.PAINTING);
+                            CompoundTag compoundtag = itemstack.getOrCreateTagElement("EntityTag");
+                            Painting.storeVariant(compoundtag, p_269979_);
+                            out.accept(itemstack);
+                        });
                     }).build());
 }
